57f0ad31ce00624ce72fc7d06f16f64231b8c788
pabloX <lonwabo.mnyaiza@gmail.com>
2026-06-01 15:47:12 +0200

    :fire::recycle: let express 5+ handle async errors.

--- src/router/genres.routes.ts
+++ src/router/genres.routes.ts
@@ -1,26 +1,21 @@
 import express, { Request, Response } from 'express';
 import authenticate from '../middleware/authentication.middleware';
 import authorize from '../middleware/authorization.middleware';
-import errorWrapper from '../utils/http-error-wrapper';
 import validateGuid from '../middleware/guid-validation.middleware';
 import { Genre, validate } from '../models/genre.model';
 
 const router = express.Router();
 const baseURL = '/api/genres';
 
-// todo: get lib to handle errors, used to be express-async-errors...
-router.get(
-  baseURL,
-  errorWrapper(async (req: Request, res: Response) => {
-    const genres = await Genre.find().sort({ name: 1 });
-    res.send(genres);
-  }),
-);
+router.get(baseURL, async (req: Request, res: Response) => {
+  const genres = await Genre.find().sort({ name: 1 });
+  res.send(genres);
+});
 
 router.get(
   `${baseURL}/:id`,
   validateGuid,
-  errorWrapper(async (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const genreId = req.guid;
     const genre = await Genre.findById(genreId);
 
@@ -28,19 +23,17 @@ router.get(
     if (!validate(req.body)) {
       res
         .status(400)
-        .send(
-          `Name property does not meet the minimum length requirements.`,
-        );
+        .send(`Name property does not meet the minimum length requirements.`);
     }
 
     res.status(200).send(genre);
-  }),
+  },
 );
 
 router.post(
   baseURL,
   [authenticate, authorize],
-  errorWrapper(async (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const { error } = validate(req.body);
     const genreName = req.body.name;
 
@@ -52,13 +45,13 @@ router.post(
     await genre.save();
 
     res.status(201).send(`Created the following genre: ${genre.name}`);
-  }),
+  },
 );
 
 router.put(
   `${baseURL}/:id`,
   [authenticate, authorize, validateGuid],
-  errorWrapper(async (req:Request, res:Response) => {
+  async (req: Request, res: Response) => {
     const { error } = validate(req.body);
     if (error) {
       res.status(400).send(error?.details[0]?.message);
@@ -75,21 +68,21 @@ router.put(
     }
 
     res.status(201).send(`Genre updated to: ${genreName}`);
-  }),
+  },
 );
 
 router.delete(
   `${baseURL}/:id`,
   [authenticate, authorize, validateGuid],
-  errorWrapper(async (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const genreId = req.guid;
     const genre = await Genre.findById(genreId);
     if (!genre) res.status(404).send('The genre id does not exist');
 
-    await Genre.deleteOne({_id: genreId});
+    await Genre.deleteOne({ _id: genreId });
 
     res.status(201).send(`Genre deleted: ${genre?.name}`);
-  }),
+  },
 );
 
 export default router;
